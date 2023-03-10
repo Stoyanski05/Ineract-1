8ff139807f287a4badb4851b43ac1a91d556ce24
Stoyanski05 <111732047+Stoyanski05@users.noreply.github.com>
2023-03-10 09:18:22 +0100

    Add files via upload

---
+++ second.js
@@ -0,0 +1,44 @@
+// øvlse3
+// var tekst = "Hey verden"
+// alert(tekst)
+// var first = "Heyyyyyyy "
+// var second = "verden"
+// var samlet = first + second;
+// alert(samlet)
+
+// øvlse 4
+// var tal1 = 5;
+// var tal2 = 16;
+// var samlet2 = tal1 + tal2;
+// alert(samlet2)
+
+// øvlse 5
+// var third ="dette er en tekst"
+// var tal3 = 1112;
+// var samlet3 = third + tal3;
+// alert(samlet3)
+
+// øvlse 6
+// var tal4 = 21;
+// var tal5 = 17;
+// alert(tal4 + tal5);
+// alert(tal4 - tal5);
+// alert(tal4 * tal5);
+// alert(tal4 / tal5);
+
+// Øvlse 7
+// var tal5 = prompt("Skriv et tal"); 
+// document.Write(tal5);
+
+// Øvlse 8
+var tal5 = prompt("Skriv et tal");
+if(IsNaN(tal5)){
+        alert(tal5 + "ER et tal");
+    
+var tal5 = prompt("Skriv et tal");
+}
+
+// Øvlse 9
+if (test) {
+    
+}
